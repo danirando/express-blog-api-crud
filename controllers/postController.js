b71a22b8a95db0029c71dcacb2b19d2785fd2ad9
danirando <danielerando@tiscali.it>
2025-05-15 14:23:46 +0200

    aggiungo logica modify in postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -66,18 +66,50 @@ const store = (req, res) => {
   posts.push(newPost);
 
   res.status(201).json(newPost);
+
+  console.log(newPost);
 };
 
 // UPDATE
 
 const update = (req, res) => {
-  res.json("sostituisco un post");
+  const postId = parseInt(req.params.id);
+  const { title, content, image, tags } = req.body;
+  const originalPost = posts.find((post) => post.id === post.id);
+
+  const updatePost = { id: postId, title, content, image, tags };
+
+  const originalPostIndex = posts.indexOf(originalPost);
+
+  posts.splice(originalPostIndex, 1, updatePost);
+
+  res.json(updatePost);
 };
 
 //  MODIFY
 
 const modify = (req, res) => {
-  res.json("modifico un post");
+  const postId = parseInt(req.params.id);
+  // const { title, content, image, tags } = req.body;
+  const originalPost = posts.find((post) => post.id === post.id);
+
+  if (!originalPost) {
+    return res
+      .status(404)
+      .json({ error: "404 not found", message: "Post non trovato" });
+  }
+
+  const title = req.body.title ?? originalPost.title;
+  const content = req.body.content ?? originalPost.content;
+  const image = req.body.image ?? originalPost.image;
+  const tags = req.body.tags ?? originalPost.tags;
+
+  originalPost.title = title;
+  originalPost.content = content;
+  originalPost.image = image;
+  originalPost.tags = tags;
+
+  res.json(originalPost);
 };
 
 // DESTROY
