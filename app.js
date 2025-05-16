e77ebf0a76627cd28c52b892118294893a746ef3
danirando <danielerando@tiscali.it>
2025-05-16 12:49:46 +0200

    aggiunta logica per gestire errori in middlewares e importata in app.js

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const port = 3000;
 app.use(express.json());
 
 const postRouter = require("./routers/posts");
+const errorHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
 
 app.use("/posts", postRouter);
 
 app.use(express.static(`public`));
+
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
   console.log("Il server è in ascolto su http://localhost:" + port);
